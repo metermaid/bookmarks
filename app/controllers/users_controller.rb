bc7c65ace70e0c643e9acbf89d771e764e5fa941
Rita Cheng <rita@ritacheng.com>
2013-10-08 22:41:11 -0700

    fixed pagination on user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   # GET /users/1
   def show
     @user = User.find(params[:id])
-    @items = Kaminari.paginate_array((@user.bookmarks + @user.comments).sort{|x,y| y.created_at <=> x.created_at}).page(params[:page])
+    @items = (@user.bookmarks + @user.comments).sort{|x,y| y.created_at <=> x.created_at}
+    @items = Kaminari.paginate_array(@items).page(params[:page]).per(7)
     @menu_item = "profile"
   end
 end
